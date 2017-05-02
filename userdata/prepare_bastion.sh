f720939912f04bacb9b927e8f7731f5f41dec378
simcap <simcap@fastmail.com>
2017-05-02 17:40:37 +0200

    Linux bastion template

---
+++ userdata/prepare_bastion.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export PATH=$PATH:/usr/local/bin
+
+which pip &> /dev/null
+
+if [ $? -ne 0 ] ; then
+    echo "PIP NOT INSTALLED"
+    [ `which yum` ] && $(yum install -y epel-release; yum install -y python-pip curl) && echo "PIP INSTALLED"
+    [ `which apt-get` ] && apt-get -y update && apt-get -y install python-pip curl && echo "PIP INSTALLED"
+fi
+
+pip install --upgrade pip &> /dev/null
+pip install awscli --ignore-installed six &> /dev/null
+easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+
+EIP_LIST="13.56.33.187,52.9.108.85"
+
+curl https://s3.amazonaws.com/quickstart-reference/linux/bastion/latest/scripts/bastion_bootstrap.sh | bash -s -- --tcp-forwarding true --enable false
