f9acb0f8cc622581adf468d08874917736efa6eb
simcap <simcap@fastmail.com>
2017-05-31 17:29:44 +0200

    Capturing correct field from csv

--- userdata/redhat/kafka.sh
+++ userdata/redhat/kafka.sh
@@ -6,7 +6,7 @@ curl -O https://raw.githubusercontent.com/wallix/awless/master/getawless.sh
 /bin/bash getawless.sh
 mv awless /usr/local/bin
 
-ZOOKEEPER_IP=$(/usr/local/bin/awless ls instances --filter name=zookeeper --format csv | tail -1 | cut -d, -f6)
+ZOOKEEPER_IP=$(/usr/local/bin/awless ls instances --filter name=zookeeper --format csv | tail -1 | cut -d, -f7)
 
 KAFKA_DOWNLOAD=kafka_2.12-0.10.2.1
 
