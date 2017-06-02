df9a442a18ee9e3c4e926e2afaa3c8fd8a5a40c4
simcap <simcap@fastmail.com>
2017-06-02 12:31:00 +0200

    Broker advertised its host port

--- userdata/redhat/kafka.sh
+++ userdata/redhat/kafka.sh
@@ -17,10 +17,12 @@ ln -s /opt/$KAFKA_DOWNLOAD /opt/kafka
 
 mkdir /tmp/kafka-logs
 
+INSTANCE_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 BROKER_ID=$(/usr/local/bin/awless ls instances --filter name=broker --sort name --format tsv | tail -n +2 | grep $INSTANCE_ID -n | cut -d: -f1)
 
-sed -i.bak "s/zookeeper.connect=.*/zookeeper.connect=$ZOOKEEPER_IP:2181/g" /opt/kafka/config/server.properties
-sed -i.bak "s/broker.id=.*/broker.id=$BROKER_ID/g" /opt/kafka/config/server.properties
+sed -i "s/zookeeper.connect=.*/zookeeper.connect=$ZOOKEEPER_IP:2181/" /opt/kafka/config/server.properties
+sed -i "s/broker.id=.*/broker.id=$BROKER_ID/" /opt/kafka/config/server.properties
+sed -i "s_#listeners=PLAINTEXT://:9092_listeners=PLAINTEXT://$INSTANCE_IP:9092_" /opt/kafka/config/server.properties
 
 nohup /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties > /dev/null 2>&1 &
