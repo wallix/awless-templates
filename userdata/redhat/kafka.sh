3981540dae3e67e2c9fd547dd8be4771cf499db4
simcap <simcap@fastmail.com>
2017-11-13 16:46:09 +0100

    Kafka: upgrading version

--- userdata/redhat/kafka.sh
+++ userdata/redhat/kafka.sh
@@ -7,9 +7,9 @@ mv awless /usr/bin
 
 ZOOKEEPER_IP=$(/usr/bin/awless ls instances --filter name=zookeeper --filter state=run --format csv | tail -1 | cut -d, -f7)
 
-KAFKA_DOWNLOAD=kafka_2.12-0.10.2.1
+KAFKA_DOWNLOAD=kafka_2.12-1.0.0
 
-curl -O http://apache.mediamirrors.org/kafka/0.10.2.1/$KAFKA_DOWNLOAD.tgz
+curl -O http://apache.mediamirrors.org/kafka/1.0.0/$KAFKA_DOWNLOAD.tgz
 
 tar -zxvf $KAFKA_DOWNLOAD.tgz -C /opt
 ln -s /opt/$KAFKA_DOWNLOAD /opt/kafka
@@ -18,7 +18,7 @@ mkdir /tmp/kafka-logs
 
 INSTANCE_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-BROKER_ID=$(/usr/bin/awless ls instances --filter name=broker --sort name --format tsv | tail -n +2 | grep $INSTANCE_ID -n | cut -d: -f1)
+BROKER_ID=$(/usr/bin/awless ls instances --filter name=broker --sort name --format tsv --no-headers | grep $INSTANCE_ID -n | cut -d: -f1)
 
 sed -i "s/zookeeper.connect=.*/zookeeper.connect=$ZOOKEEPER_IP:2181/" /opt/kafka/config/server.properties
 sed -i "s/broker.id=.*/broker.id=$BROKER_ID/" /opt/kafka/config/server.properties
--- userdata/redhat/zookeeper.sh
+++ userdata/redhat/zookeeper.sh
@@ -2,9 +2,9 @@
 
 yum -y install java
 
-KAFKA_DOWNLOAD=kafka_2.12-0.10.2.1
+KAFKA_DOWNLOAD=kafka_2.12-1.0.0
 
-curl -O http://apache.mediamirrors.org/kafka/0.10.2.1/$KAFKA_DOWNLOAD.tgz
+curl -O http://apache.mediamirrors.org/kafka/1.0.0/$KAFKA_DOWNLOAD.tgz
 
 tar -zxvf $KAFKA_DOWNLOAD.tgz -C /opt
 ln -s /opt/$KAFKA_DOWNLOAD /opt/kafka
